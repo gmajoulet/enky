3765a26ac119122e37867992f5a52219acd61a23
Gmajoulet <gmajoulet@gmail.com>
2015-05-14 12:06:09 +0200

    Escrow controller and model

---
+++ api/controllers/EscrowController.js
@@ -0,0 +1,67 @@
+/**
+ * EscrowController
+ *
+ * @description :: Server-side logic for managing escrows
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+  /**
+   * `EscrowController.get()`
+   */
+  get: function (req, res) {
+    return res.json({
+      todo: 'get() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `EscrowController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `EscrowController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `EscrowController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `EscrowController.release()`
+   */
+  release: function (req, res) {
+    return res.json({
+      todo: 'release() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `EscrowController.refund()`
+   */
+  refund: function (req, res) {
+    return res.json({
+      todo: 'refund() is not implemented yet!'
+    });
+  }
+};
---
+++ api/models/Escrow.js
@@ -0,0 +1,17 @@
+/**
+* Escrow.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+  attributes: {
+    get: { type: 'string' },
+    create: { type: 'string' },
+    update: { type: 'string' },
+    delete: { type: 'string' },
+    release: { type: 'string' },
+    refund: { type: 'string' }
+  }
+};
